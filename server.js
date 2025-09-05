6e749561341082b9c9874aa909eb28e3425b2469
Floxde99 <florian.fchr@hotmail.com>
2025-09-05 16:10:10 +0200

    test2 427

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', userRouter);
+app.use('/contact', contactRouter); // Ajouter cette ligne
 
 app.listen(process.env.PORT, (err) => {
     if (err) {
